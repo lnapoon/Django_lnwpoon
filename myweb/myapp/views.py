895fb4f498d9c4b0d3db48d1e055faef942650cc
lnapoon <mpoontv1234@gmail.com>
2026-07-13 11:24:17 +0700

    Fix mismatched html divs in templates and views mappings

--- myweb/myapp/views.py
+++ myweb/myapp/views.py
@@ -5,7 +5,7 @@ def index(request):
     return render(request, "index.html")
 
 def about(request):
-    return HttpResponse("About Page")
+    return render(request, "about.html")
 
 def contact(request):
-    return HttpResponse("Contact Page")
+    return render(request, "contact.html")
